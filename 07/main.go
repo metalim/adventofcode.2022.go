ac2785c95dc2e1b988ee3c335931d957db984d58
Maksim Litvinov <metalim@gmail.com>
2023-12-07 18:28:57 +0200

    add day 7 part 1

---
+++ 07/main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func catch(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: go run main.go input.txt")
+		os.Exit(1)
+	}
+
+	bs, err := os.ReadFile(os.Args[1])
+	catch(err)
+	lines := strings.Split(string(bs), "\n")
+
+	part1(lines)
+	part2(lines)
+}
+
+type Dir struct {
+	parent *Dir
+	dirs   map[string]*Dir
+	files  map[string]int
+}
+
+func (d *Dir) cd(name string) *Dir {
+	if name == "/" {
+		for d.parent != nil {
+			d = d.parent
+		}
+		return d
+	}
+
+	if name == ".." {
+		return d.parent
+	}
+
+	return d.dirs[name]
+}
+
+func (d *Dir) mkdir(path string) {
+	d.dirs[path] = &Dir{
+		parent: d,
+		dirs:   map[string]*Dir{},
+		files:  map[string]int{},
+	}
+}
+
+func (d *Dir) touch(path string, size int) {
+	d.files[path] = size
+}
+
+func (d *Dir) getSize() int {
+	var total int
+	for _, size := range d.files {
+		total += size
+	}
+	for _, dir := range d.dirs {
+		total += dir.getSize()
+	}
+	return total
+}
+
+func (d *Dir) sumDirsLessThan(maxSize int) int {
+	var total int
+
+	size := d.getSize()
+	if size <= maxSize {
+		total += size
+	}
+
+	for _, dir := range d.dirs {
+		total += dir.sumDirsLessThan(maxSize)
+	}
+	return total
+}
+
+func part1(lines []string) {
+	root := &Dir{
+		dirs:  map[string]*Dir{},
+		files: map[string]int{},
+	}
+
+	cwd := root
+	for _, line := range lines {
+		switch {
+		case strings.HasPrefix(line, "$ cd "):
+			cwd = cwd.cd(line[5:])
+		case strings.HasPrefix(line, "$ ls"):
+			// do nothing
+		case strings.HasPrefix(line, "dir "):
+			cwd.mkdir(line[4:])
+
+		default:
+			entry := strings.Fields(line)
+			size, err := strconv.Atoi(entry[0])
+			catch(err)
+			cwd.touch(entry[1], size)
+		}
+	}
+	fmt.Println("Part 1:", root.sumDirsLessThan(1e5))
+}
+
+func part2(lines []string) {
+}
