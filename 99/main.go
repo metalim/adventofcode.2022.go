def7be3e030fd45f53dc857f6fadb00b79c40a24
Maksim Litvinov <metalim@gmail.com>
2024-12-18 19:47:23 +0200

    update template

--- 99/main.go
+++ 99/main.go
@@ -17,42 +17,42 @@ func catch(err error) {
 func main() {
 	flag.Parse()
 	if flag.NArg() != 1 {
-		fmt.Println("Usage: go run main.go input.txt")
+		fmt.Println("Usage: go run . input.txt")
 		os.Exit(1)
 	}
 
 	bs, err := os.ReadFile(flag.Arg(0))
 	catch(err)
 
-	input := parseInput(string(bs))
-	part1(input)
-	part2(input)
+	parsed := parseInput(string(bs))
+	part1(parsed)
+	part2(parsed)
 }
 
-type Input []string
+type Parsed []string
 
-func parseInput(input string) Input {
+func parseInput(input string) Parsed {
 	lines := strings.Split(input, "\n")
 	if len(lines[len(lines)-1]) == 0 {
 		lines = lines[:len(lines)-1]
 	}
-	return Input(lines)
+	return Parsed(lines)
 }
 
-func part1(input Input) {
+func part1(parsed Parsed) {
 	timeStart := time.Now()
-	for _, line := range input {
+	for _, line := range parsed {
 		fmt.Println(line)
 	}
 
-	fmt.Printf("Part 1: \t\tin %v\n", time.Since(timeStart))
+	fmt.Printf("Part 1: %d\t\tin %v\n", 0, time.Since(timeStart))
 }
 
-func part2(input Input) {
+func part2(parsed Parsed) {
 	timeStart := time.Now()
-	for _, line := range input {
+	for _, line := range parsed {
 		_ = line
 	}
 
-	fmt.Printf("Part 2: \t\tin %v\n", time.Since(timeStart))
+	fmt.Printf("Part 2: %d\t\tin %v\n", 0, time.Since(timeStart))
 }
